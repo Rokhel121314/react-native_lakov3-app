040b65312ef47cabf61200021a83123c9084a231
jerickwebdev <jerickddelacruzwebdev@gmail.com>
2023-06-16 12:14:18 +0800

    first commit

---
+++ src/screens/stack-screen/ViewTransactionScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ViewTransactionScreen = () => {
+  return (
+    <View>
+      <Text>ViewTransactionScreen</Text>
+    </View>
+  );
+};
+
+export default ViewTransactionScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/tab-screen/PosScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const PosScreen = () => {
+  return (
+    <View>
+      <Text>PosScreen</Text>
+    </View>
+  );
+};
+
+export default PosScreen;
+
+const styles = StyleSheet.create({});
