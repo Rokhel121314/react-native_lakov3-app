d564338e66e68b44748f33760b9469b8740767c1
jerickwebdev <jerickddelacruzwebdev@gmail.com>
2023-06-20 14:47:38 +0800

    implemented getAllProduct on upon logged in, display all product on stock screen

---
+++ src/hooks/useProduct.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const useProduct = () => {
+  return {};
+};
+
+export default useProduct;
--- src/redux/store.js
+++ src/redux/store.js
@@ -15,27 +15,42 @@ import {
 } from "redux-persist";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import userSlice from "./userSlice";
+import productSlice from "./productSlice";
 
 const rootReducer = combineReducers({
   user: userSlice,
+  product: productSlice,
 });
 
 const persistConfig = {
   key: "user",
   version: 1,
   storage: AsyncStorage,
-  blacklist: [],
+  blacklist: [productSlice],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+// const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: getDefaultMiddleware({
+//     serializableCheck: {
+//       ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//     },
+//     immutableCheck: {warnAfter: 128}
+//   }),
+// });
+
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: getDefaultMiddleware({
-    serializableCheck: {
-      ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      immutableCheck: { warnAfter: 128 },
+      serializableCheck: {
+        warnAfter: 128,
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
