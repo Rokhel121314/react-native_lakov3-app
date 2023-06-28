3c50828d72fa89b5115345e3fdf99023302bbfe6
jerickwebdev <jerickddelacruzwebdev@gmail.com>
2023-06-28 13:32:40 +0800

    implemented share image of receipt, save image of receipt to device using expo share, expo media library and react native view shot package

---
+++ src/components/SaveImageConfirmationModal.jsx
@@ -0,0 +1,99 @@
+import {
+  View,
+  Text,
+  Modal,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { AntDesign } from "@expo/vector-icons";
+
+const SaveImageConfirmationModal = (props) => {
+  //
+  const {
+    modalVisible,
+    setModalVisible,
+    handleConfirmAction,
+    confirmationMessage,
+    confirmBtnText,
+    cancelBtnText,
+    isSaved,
+    setIsSaved,
+    successMessage,
+  } = props;
+
+  const [saving, setSaving] = useState(false);
+
+  const saveLoading = async () => {
+    await setSaving(true);
+    setTimeout(async () => {
+      await setSaving(false);
+      setIsSaved(true);
+      setTimeout(() => {
+        setIsSaved(false);
+        setModalVisible(false);
+      }, 1000);
+    }, 1000);
+  };
+
+  return (
+    <Modal
+      transparent={true}
+      animationType="slide"
+      visible={modalVisible}
+      onRequestClose={() => {
+        setModalVisible(false);
+      }}>
+      <View className="flex-1 items-center justify-center z-10 relative bg-blue-dianne-blur">
+        {saving ? (
+          <View className="h-60 w-80 justify-center bg-gray-50 items-center z-0 absolute opacity-100 rounded-xl px-10">
+            <ActivityIndicator size={"large"} color={"#344c57"} />
+          </View>
+        ) : (
+          <>
+            {!isSaved ? (
+              <View className="h-60 w-80 justify-center bg-gray-50 items-center z-0 absolute opacity-100 rounded-xl px-10">
+                <Text
+                  className="text-blue-dianne text-lg font-bold text-center"
+                  numberOfLines={2}>
+                  {`${confirmationMessage}`}
+                </Text>
+                <View className="flex-row justify-around w-full mt-10">
+                  <TouchableOpacity
+                    className="rounded-lg border border-blue-dianne"
+                    onPress={() => setModalVisible(false)}>
+                    <Text className="px-4 py-1 text-blue-dianne text-lg font-bold">
+                      {cancelBtnText}
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    className="rounded-lg  bg-blue-dianne"
+                    onPress={() => {
+                      saveLoading();
+                      handleConfirmAction();
+                    }}>
+                    <Text className="px-4 py-1 text-gray-50 text-lg font-bold">
+                      {confirmBtnText}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ) : (
+              <View className="h-60 w-80 justify-center bg-gray-50 items-center z-0 absolute opacity-100 rounded-xl px-10">
+                <Text
+                  className="text-blue-dianne text-lg font-bold text-center mb-10"
+                  numberOfLines={2}>
+                  {`${successMessage}`}
+                </Text>
+                <AntDesign name="checkcircleo" size={45} color="#344c57" />
+              </View>
+            )}
+          </>
+        )}
+      </View>
+    </Modal>
+  );
+};
+
+export default SaveImageConfirmationModal;
--- src/screens/stack-screen/ViewTransactionScreen.jsx
+++ src/screens/stack-screen/ViewTransactionScreen.jsx
@@ -1,93 +1,153 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  ScrollView,
-  TouchableOpacity,
-} from "react-native";
-import React from "react";
+import { Text, View, Image, ScrollView, TouchableOpacity } from "react-native";
+import React, { useRef, useEffect, useState, useCallback } from "react";
 import { useRoute } from "@react-navigation/native";
 import { useSelector } from "react-redux";
 import SoldItem from "../../components/SoldItem";
+import ViewShot from "react-native-view-shot";
+import * as MediaLibrary from "expo-media-library";
+import { shareAsync } from "expo-sharing";
+import SaveImageConfirmationModal from "../../components/SaveImageConfirmationModal";
 
 const ViewTransactionScreen = ({ navigation }) => {
   const route = useRoute();
+  const [photo, setPhoto] = useState();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [isSaved, setIsSaved] = useState(false);
+  const viewRef = useRef();
   const { userData } = useSelector((state) => state.user);
 
   const { item } = route.params;
 
   const soldItems = item.transaction_sold_items;
-  console.log("soldItems", item);
+
+  // saving image to phone
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
+
+  const reqMediaLibrabryPermission = async () => {
+    const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+    setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+  };
+
+  const onCapture = useCallback((uri) => {
+    setPhoto(uri);
+  }, []);
+
+  useEffect(() => {
+    reqMediaLibrabryPermission();
+  }, []);
+
+  const saveImage = async () => {
+    try {
+      await MediaLibrary.saveToLibraryAsync(photo);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const shareImage = async () => {
+    await shareAsync(photo);
+  };
 
   return (
-    <ScrollView
-      className="bg-gray-100 px-5"
-      showsVerticalScrollIndicator={false}>
-      <View className="flex-auto items-center">
-        <View className=" items-center justify-center">
-          <Image
-            source={{ uri: userData.store_logo }}
-            resizeMode="contain"
-            style={{ width: 75, height: 75 }}
-          />
-          <Text className="font-bold text-xl">{userData.store_name}</Text>
+    <>
+      <SaveImageConfirmationModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        handleConfirmAction={saveImage}
+        confirmationMessage={"SAVE RECEIPT TO YOUR DEVICE?"}
+        confirmBtnText={"CONFIRM"}
+        cancelBtnText={"CANCEL"}
+        isSaved={isSaved}
+        setIsSaved={setIsSaved}
+        productDetail={""}
+        successMessage={"SAVED SUCCESSFULLY!"}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <ViewShot
+          className="bg-gray-100 px-5"
+          onCapture={onCapture}
+          captureMode="mount"
+          ref={viewRef}
+          options={{
+            fileName: "lako-receipt",
+            format: "png",
+            quality: 0.9,
+          }}>
+          <View className="flex-auto items-center">
+            <View className=" items-center justify-center">
+              <Image
+                source={{ uri: userData.store_logo }}
+                resizeMode="contain"
+                style={{ width: 75, height: 75 }}
+              />
+              <Text className="font-bold text-xl">{userData.store_name}</Text>
+            </View>
+            <Text>ADDRESS</Text>
+            <Text>CONTACT NUMBER</Text>
+            <Text>{`Transaction #: ${item._id}`}</Text>
+            <Text>{`Date: ${item.createdAt}`}</Text>
+          </View>
+          <View className="flex-auto">
+            <View className="flex-row justify-between border-b border-blue-dianne mt-10 px-5">
+              <Text className="font-bold text-xl">Description</Text>
+              <Text className="font-bold text-xl">Total</Text>
+            </View>
+            <View className="border-b border-blue-dianne">
+              {soldItems.map((items) => {
+                return <SoldItem items={items} key={items._id} />;
+              })}
+            </View>
+          </View>
+          <View className="flex-auto mb-5">
+            <View className="justify-between items-center px-5 flex-row">
+              <Text className="text-gray-950 text-lg font-semibold">Items</Text>
+              <Text className="text-gray-950 text-lg font-semibold">
+                {`${item.transaction_sold_quantity.toFixed(2)} pcs`}
+              </Text>
+            </View>
+            <View className="justify-between items-center px-5 flex-row">
+              <Text className="text-gray-950 text-lg font-semibold">Total</Text>
+              <Text className="text-gray-950 text-lg font-semibold">
+                {`-$ ${item.transaction_sold_amount.toFixed(2)}`}
+              </Text>
+            </View>
+            <View className="justify-between items-center px-5 flex-row">
+              <Text className="text-gray-950 text-lg font-semibold">
+                Payment
+              </Text>
+              <Text className="text-gray-950 text-lg font-semibold">
+                {`$ ${item.transaction_payment_amount.toFixed(2)}`}
+              </Text>
+            </View>
+            <View className="justify-between items-center px-5 flex-row ">
+              <Text className="text-gray-950 text-lg font-semibold">
+                Payment Change
+              </Text>
+              <Text className="text-gray-950 text-lg font-semibold">
+                {`$ ${item.transaction_payment_change.toFixed(2)}`}
+              </Text>
+            </View>
+          </View>
+        </ViewShot>
+        <View className="flex-1 px-5 mt-5 mb-5 ">
+          <TouchableOpacity
+            className="border-2 border-blue-dianne items-center "
+            onPress={() => setModalVisible(true)}>
+            <Text className="py-3 text-blue-dianne text-base font-bold tracking-widest">
+              SAVE
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            className="bg-blue-dianne items-center mt-3"
+            onPress={shareImage}>
+            <Text className="py-3 text-gray-100 text-base font-bold tracking-widest">
+              SHARE
+            </Text>
+          </TouchableOpacity>
         </View>
-        <Text>ADDRESS</Text>
-        <Text>CONTACT NUMBER</Text>
-        <Text>{`Transaction #: ${item._id}`}</Text>
-        <Text>{`Date: ${item.createdAt}`}</Text>
-      </View>
-      <View className="flex-auto">
-        <View className="flex-row justify-between border-b border-blue-dianne mt-10 px-5">
-          <Text className="font-bold text-xl">Description</Text>
-          <Text className="font-bold text-xl">Total</Text>
-        </View>
-        <View className="border-b border-blue-dianne">
-          {soldItems.map((items) => {
-            return <SoldItem items={items} />;
-          })}
-        </View>
-      </View>
-      <View className="flex-auto mb-5">
-        <View className="justify-between items-center px-5 flex-row">
-          <Text className="text-gray-950 text-lg font-semibold">Items</Text>
-          <Text className="text-gray-950 text-lg font-semibold">
-            {`${item.transaction_sold_quantity.toFixed(2)} pcs`}
-          </Text>
-        </View>
-        <View className="justify-between items-center px-5 flex-row">
-          <Text className="text-gray-950 text-lg font-semibold">Total</Text>
-          <Text className="text-gray-950 text-lg font-semibold">
-            {`-$ ${item.transaction_sold_amount.toFixed(2)}`}
-          </Text>
-        </View>
-        <View className="justify-between items-center px-5 flex-row">
-          <Text className="text-gray-950 text-lg font-semibold">Payment</Text>
-          <Text className="text-gray-950 text-lg font-semibold">
-            {`$ ${item.transaction_payment_amount.toFixed(2)}`}
-          </Text>
-        </View>
-        <View className="justify-between items-center px-5 flex-row ">
-          <Text className="text-gray-950 text-lg font-semibold">
-            Payment Change
-          </Text>
-          <Text className="text-gray-950 text-lg font-semibold">
-            {`$ ${item.transaction_payment_change.toFixed(2)}`}
-          </Text>
-        </View>
-      </View>
-      <View className="flex-1 px-5 mt-5 mb-5">
-        <TouchableOpacity className="bg-blue-dianne items-center">
-          <Text className="py-3 text-gray-100 text-base font-bold tracking-widest">
-            PRINT
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </>
   );
 };
 
 export default ViewTransactionScreen;
-
-const styles = StyleSheet.create({});
